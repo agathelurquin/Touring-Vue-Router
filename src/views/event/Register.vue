6009ad36f36a8428854dc94acfa125413f32f7d8
agathelurquin <agathe.lurquin@essca.eu>
2023-09-07 11:43:45 +0200

    programmatic Navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,18 @@
 <template>
   <p>Register to the Event</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      this.$router.push({
+        name: 'About'
+        // params populated by default from the parent root URL
+      })
+    }
+  }
 }
 </script>
